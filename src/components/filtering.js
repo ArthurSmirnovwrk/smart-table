0ff6c3396aa7cd47e3aa528eb694ecf73c8f890c
Arthur Smirnov <aakterhall@yandex.ru>
2025-09-14 20:16:39 +0300

    Сделал шаги 2-5

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,14 +1,50 @@
 import {createComparison, defaultRules} from "../lib/compare.js";
 
 // @todo: #4.3 — настроить компаратор
+const compare = createComparison([
+	'skipEmptyTargetValues',
+	'caseInsensitiveStringIncludes',
+	'skipNonExistentSourceFields',
+	'failOnEmptySource',
+	'arrayAsRange',
+	'stringIncludes',
+	'exactEquality'
+]);
 
 export function initFiltering(elements, indexes) {
     // @todo: #4.1 — заполнить выпадающие списки опциями
+    Object.keys(indexes).forEach((elementName) => {
+		if (elements[elementName]) {
+			const options = Object.values(indexes[elementName]).map(name => {
+				const option = document.createElement('option');
+				option.value = name;
+				option.textContent = name;
+				return option;
+			});
+
+			const emptyOption = document.createElement('option');
+			emptyOption.value = '';
+			emptyOption.textContent = 'Все';
+			elements[elementName].append(emptyOption, ...options);
+		}
+	});
 
     return (data, state, action) => {
         // @todo: #4.2 — обработать очистку поля
+        if (action && action.name === 'clear') {
+			const fieldName = action.dataset.field;
+			const parent = action.closest('.filter-group');
+
+			if (parent) {
+				const input = parent.querySelector('input, select');
+				if (input) {
+					input.value = '';
+					state[fieldName] = '';
+				}
+			}
+		}
 
         // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+        return data.filter(row => compare(row, state));
     }
 }
--- src/components/searching.js
+++ src/components/searching.js
@@ -3,9 +3,20 @@ import {rules, createComparison} from "../lib/compare.js";
 
 export function initSearching(searchField) {
     // @todo: #5.1 — настроить компаратор
+    const searchRule = rules.searchMultipleFields(
+		searchField,
+		['date', 'customer', 'seller', 'total'],
+		false
+	);
+    
+    const compare = createComparison(['skipEmptyTargetValues'], [searchRule]);
 
     return (data, state, action) => {
         // @todo: #5.2 — применить компаратор
-        return data;
+        let filter_data = data.filter(row => {
+			// Применяем компаратор
+			return compare(row, state);
+		});
+		return filter_data;
     }
 }
